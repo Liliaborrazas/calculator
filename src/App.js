0d62462bcce14dd1b5d5be4b9b0da1773b644142
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2020-09-22 11:16:39 +0200

    componente numbers done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Button from './components/button/Button';
 import Functions from './components/Functions';
 import MathOperations from './components/MathOperations';
 import Numbers from './components/Numbers'
@@ -12,28 +11,13 @@ const App =()=> {
       <header >
         <main className="react-calculator">Calculadora App
           <Result value={0}/>
-            {/* <div className="numbers">
-               <Button text={1} handleClick={ text=>{
-                  console.log(`estoy dentro del handle ${text}`)
-               }}/>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button>
-            </div> */}
-            <Numbers onClickNumbers={number=>{console.log(`hice click en el : ${number}`)}}/>
+            <Numbers onClickNumber={number=>{console.log(`hice click en el : ${number}`)}}/>
             <Functions 
              onContentClear={clear=>{console.log(`clear :${clear}`)}}
              onDelete={del=>{console.log(`Delete:${del}`)}}/>
             <MathOperations 
             onClickOperations={operations =>console.log(`Soy el parametro o sea el texto del btn q haces click soy: ${operations}`)}
             onClickEqual={equal =>console.log(`Soy el paramentro equal definido en MathOpertion si haces click te pintara:${equal}`)}
-            
             />
             </main>
       </header>
--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from './button/Button';
 
-const numbers = [1,2,3,4,5,6,7,8,9,0]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 const renderButtons = onClickNumber => {
-    //iterar desde el botón 1 al 0
-    //<Button text={number.toString()} handleClick={onClickNumbers}/>
     const renderButton = number => (
-        <Button text={number.toString()} handleClick={onClickNumber}/>
+        <Button
+             key={number} 
+             text={number.toString()} 
+             handleClick={onClickNumber}
+        />
     )
     return numbers.map(renderButton)
 }
