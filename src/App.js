5916dac408a05aac78b15d86f5e2803416d47023
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2020-09-24 13:40:02 +0200

    calculadora funcionando

--- src/App.js
+++ src/App.js
@@ -13,14 +13,27 @@ const App =()=> {
         <main className="react-calculator">Calculadora App
           <Result value={stack}/>
             <Numbers onClickNumber={number=>{console.log(`hice click en el : ${number}`)
-          setStack(stack + number)
+          setStack(`${stack}${number}`)
           }}/>
             <Functions 
-             onContentClear={clear=>{console.log(`clear :${clear}`)}}
-             onDelete={del=>{console.log(`Delete:${del}`)}}/>
+              onContentClear={clear=>{
+                  console.log(`clear :${clear}`)
+                  setStack("")
+              }}
+             onDelete={del=>{
+                if(stack.length > 0){ 
+                    const newStack = stack.substring(0, stack.length -1)
+                    console.log(`Delete:${newStack}`)
+                    setStack(newStack)
+                } 
+             }}/>
             <MathOperations 
-            onClickOperations={operations =>console.log(`Soy el parametro o sea el texto del btn q haces click soy: ${operations}`)}
-            onClickEqual={equal =>console.log(`Soy el paramentro equal definido en MathOpertion si haces click te pintara:${equal}`)}
+            onClickOperations={operations =>{console.log(`Soy el parametro o sea el texto del btn q haces click soy: ${operations}`)
+            setStack(`${stack}${operations}`)
+          }}
+            onClickEqual={equal =>{console.log(`Soy el paramentro equal definido en MathOpertion si haces click te pintara:${equal}`)
+            setStack(eval(stack))
+          }}
             />
             </main>
       </header>
